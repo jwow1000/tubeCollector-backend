870cbffc4415aac420cbcbc30ce31274257de871
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-05-14 17:40:20 -0400

    home route and view set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+# define the home view
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to tubeCollector!'}
+    return Response(content)
